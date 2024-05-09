35b87187346217e6f6bff38b927634b01fb8a27e
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-09 23:45:07 +0700

    add: page detail rooms

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { ThemeProvider } from "./components/providers/theme-provider.tsx";
 import CreateRoomPage from "./pages/CreateRoomPage.tsx";
 import { AuthWrapper } from "./hooks/session-provider.tsx";
 import RoomsPage from "./pages/RoomsPage.tsx";
+import DetailRoomPage from "./pages/DetailRoomPage.tsx";
 
 axios.defaults.withCredentials = true;
 const router = createBrowserRouter([
@@ -34,6 +35,10 @@ const router = createBrowserRouter([
     path: "/rooms",
     element: <RoomsPage />,
   },
+  {
+    path: "/rooms/:id",
+    element: <DetailRoomPage />,
+  }
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
