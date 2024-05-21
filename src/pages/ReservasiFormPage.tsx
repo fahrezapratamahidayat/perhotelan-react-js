9501339c7d8636f78ca3761cd0a3877578a0b2ad
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-21 22:39:26 +0700

    add: reservation form page

---
+++ src/pages/ReservasiFormPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navigation/navbar";
+import React from "react";
+
+export default function ReservasiFormPage() {
+  console.log("reservasi form");
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-col justify-around mx-8 lg:flex-row md:py-24 lg:py-4 lg:p-4">
+        <h1>reservasi form</h1>
+      </main>
+    </>
+  );
+}
