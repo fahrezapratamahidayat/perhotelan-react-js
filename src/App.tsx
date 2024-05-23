617571e70061d39cb1c2ba68688273a649b270f7
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-23 22:24:57 +0700

    create: routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,20 @@ import RoomsPage from "./pages/RoomsPage";
 import DetailRoomPage from "./pages/DetailRoomPage";
 import ReservasiFormPage from "./pages/ReservasiFormPage";
 import PaymentPage from "./pages/PaymentPage";
+import LoginForm from "./components/form/login-form";
+import RegisterForm from "./components/form/register-form";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="rooms" element={<RoomsPage />}>
-          <Route path=":id" element={<DetailRoomPage />}>
-            <Route path="reserve" element={<ReservasiFormPage />}>
-              <Route path="payment" element={<PaymentPage />} />
-            </Route>
-          </Route>
-        </Route>
+        <Route path="rooms" element={<RoomsPage />} />
+        <Route path="rooms/:id" element={<DetailRoomPage />} />
+        <Route path="rooms/:id/reserve" element={<ReservasiFormPage />} />
+        <Route path="rooms/:id/reserve/payment" element={<PaymentPage />} />
+        <Route path="/auth/login" element={<LoginForm />} />
+        <Route path="/auth/register" element={<RegisterForm />} />
       </Routes>
     </Router>
   );
