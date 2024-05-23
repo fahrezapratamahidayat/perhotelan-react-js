52663793f714afa267e09b5736b414a4b4eaf4eb
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-23 22:26:25 +0700

    update: session with types

--- src/hooks/use-session.ts
+++ src/hooks/use-session.ts
@@ -2,9 +2,10 @@ import { create } from 'zustand';
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
 import { useEffect } from 'react';
+import { userData } from '@/types';
 
 interface UserState {
-  userData: any;
+  userData: userData | null;
   setUserData: (data: any) => void;
   checkUserToken: () => { login: boolean };
   signOut: () => void;
@@ -16,7 +17,7 @@ const useUserStore = create<UserState>((set, get) => ({
   checkUserToken: () => {
     const token = Cookies.get('token');
     if (token) {
-      const decodedData = jwtDecode(token);
+      const decodedData = jwtDecode<userData>(token); // Gunakan generic untuk memastikan tipe decodedData
       // Hanya update state jika data yang didecode berbeda dengan userData saat ini
       if (JSON.stringify(get().userData) !== JSON.stringify(decodedData)) {
         set({ userData: decodedData });
