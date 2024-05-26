c5747a16933de6d6b149d59f5d5843ce124ec460
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-26 21:18:23 +0700

    add: function formatdance

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { format, parseISO } from "date-fns";
+import { id } from "date-fns/locale";
 export function formatCurrency(value: number): string {
     if (value >= 1000000) {
         return ` ${value} JT`;
@@ -12,10 +14,13 @@ export function formatCurrency(value: number): string {
 }
 
 export const formatPhoneNumber = (phoneNumber: string) => {
-  const cleaned = ('' + phoneNumber).replace(/\D/g, '');
-  const match = cleaned.match(/^(\d{2})(\d{4})(\d{3,})$/);
-  if (match) {
-    return `+${match[1]} ${match[2]} ${match[3]}`;
-  }
-  return phoneNumber;
+    const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{2})(\d{4})(\d{3,})$/);
+    if (match) {
+        return `+${match[1]} ${match[2]} ${match[3]}`;
+    }
+    return phoneNumber;
 };
+export const formatDate = (date: string, dateFormat: string) => {
+    return format(parseISO(date), dateFormat, { locale: id });
+}
