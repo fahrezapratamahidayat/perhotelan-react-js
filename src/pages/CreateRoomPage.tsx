7fb086016f9e56abd67f82f076576e9e55650952
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-30 21:45:37 +0700

    update: create room page

--- src/pages/CreateRoomPage.tsx
+++ src/pages/CreateRoomPage.tsx
@@ -17,66 +17,12 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Link } from "react-router-dom";
 import { AvatarDropDown } from "@/components/dropdown/avatar-dropdown";
 import { FormCreateRoom } from "@/components/form/form-create-room";
+import NavbarAdmin from "@/components/navigation/navbarAdmin";
 
 export default function CreateRoomPage() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex flex-col h-full max-h-screen gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link to="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="w-6 h-6" />
-              <span className="">Luxury Hotel</span>
-            </Link>
-            <Button variant="outline" size="icon" className="w-8 h-8 ml-auto">
-              <Bell className="w-4 h-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                to="#"
-                className="flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-              >
-                <Home className="w-4 h-4" />
-                Dashboard
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-              >
-                <ShoppingCart className="w-4 h-4" />
-                Orders
-                <Badge className="flex items-center justify-center w-6 h-6 ml-auto rounded-full shrink-0">
-                  6
-                </Badge>
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 px-3 py-2 transition-all rounded-lg bg-muted text-primary hover:text-primary"
-              >
-                <Package className="w-4 h-4" />
-                Rooms{" "}
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-              >
-                <Users className="w-4 h-4" />
-                Customers
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-              >
-                <LineChart className="w-4 h-4" />
-                Analytics
-              </Link>
-            </nav>
-          </div>
-        </div>
-      </div>
+      <NavbarAdmin />
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
@@ -155,9 +101,6 @@ export default function CreateRoomPage() {
           <AvatarDropDown />
         </header>
         <main className="grid items-start flex-1 gap-4 p-4 mt-5 sm:px-6 sm:py-0 md:gap-8">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Buat Kamar</h1>
-          </div>
           <FormCreateRoom />
         </main>
       </div>
