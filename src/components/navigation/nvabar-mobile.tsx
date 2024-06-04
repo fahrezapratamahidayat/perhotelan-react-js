67f00f41fc579984f36c9284d5a949b0d96fb7a0
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-06-04 22:32:07 +0700

    add: navbar-mobile

---
+++ src/components/navigation/nvabar-mobile.tsx
@@ -0,0 +1,115 @@
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
+import { File, ListFilter, PlusCircle } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Link, useNavigate } from "react-router-dom";
+import useSWR, { useSWRConfig } from "swr";
+import axios from "axios";
+import TableRooms from "@/components/(admin)/table-data/table-rooms";
+import { AvatarDropDown } from "@/components/dropdown/avatar-dropdown";
+import NavbarAdmin from "@/components/navigation/navbarAdmin";
+
+export function NavbarMobile() {
+  return (
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="w-5 h-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  to="#"
+                  className="flex gap-2 items-center text-lg font-semibold"
+                >
+                  <Package2 className="w-6 h-6" />
+                  <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Link
+                  to="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Home className="w-5 h-5" />
+                  Dashboard
+                </Link>
+                <Link
+                  to="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <ShoppingCart className="w-5 h-5" />
+                  Orders
+                  <Badge className="flex justify-center items-center ml-auto w-6 h-6 rounded-full shrink-0">
+                    6
+                  </Badge>
+                </Link>
+                <Link
+                  to="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Package className="w-5 h-5" />
+                  Rooms
+                </Link>
+                <Link
+                  to="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Users className="w-5 h-5" />
+                  Customers
+                </Link>
+                <Link
+                  to="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <LineChart className="w-5 h-5" />
+                  Analytics
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <div className="flex-1 w-full">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search Rooms..."
+                  className="pl-8 w-full shadow-none appearance-none bg-background md:w-2/3 lg:w-1/3"
+                />
+              </div>
+            </form>
+          </div>
+          <AvatarDropDown />
+        </header>
+  );
+}
