15750c3a470bf4f17d0c5bcbfcafa4c88c0ccc84
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-06-09 20:41:57 +0700

    update-table custumers

--- src/components/table/table-customer.tsx
+++ src/components/table/table-customer.tsx
@@ -32,7 +32,7 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
-import { ChevronDown, Filter } from "lucide-react";
+import { ChevronDown, Filter, PlusCircle } from "lucide-react";
 import { cn } from "@/utils/cn";
 
 interface Props {
@@ -143,146 +143,158 @@ export default function TableCustomer<TData extends reservasiTypes, TValue>({
     },
   });
   return (
-    <Card x-chunk="dashboard-05-chunk-3">
-      <CardHeader className="px-7">
-        <CardTitle>Reservasi Hotel</CardTitle>
-        <CardDescription>Reservasi terbaru dari hotel Anda.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-wrap gap-2 pb-4 lg:flex-row lg:items-center">
-          <Input
-            placeholder="Cari Reservasi disini..."
-            value={(table.getState().globalFilter as string) ?? ""}
-            onChange={(event) => {
-              const value = event.target.value;
-              table.setGlobalFilter(value);
-            }}
-            className="max-w-sm"
-          />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="lg:ml-auto">
-                Columns <ChevronDown className="w-4 h-4 ml-1 mt-0.5" />
+    <>
+      <Card x-chunk="dashboard-05-chunk-3">
+        <CardHeader className="px-7">
+          <CardTitle>Reservasi Hotel</CardTitle>
+          <CardDescription>Reservasi terbaru dari hotel Anda.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-wrap gap-2 pb-4 lg:flex-row lg:items-center">
+            <Input
+              placeholder="Cari Reservasi disini..."
+              value={(table.getState().globalFilter as string) ?? ""}
+              onChange={(event) => {
+                const value = event.target.value;
+                table.setGlobalFilter(value);
+              }}
+              className="max-w-[250px]"
+            />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="lg:ml-auto">
+                  Columns <ChevronDown className="w-4 h-4 ml-1 mt-0.5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                {table
+                  .getAllColumns()
+                  .filter((column) => column.getCanHide())
+                  .map((column) => {
+                    return (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className={cn(`capitalize`, {})}
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
+                      >
+                        {column.id}
+                      </DropdownMenuCheckboxItem>
+                    );
+                  })}
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="">
+                  <Filter className="w-4 h-4 mr-2" />
+                  Filter Status
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onSelect={() => setStatusFilter("")}>
+                  <DropdownMenuCheckboxItem
+                    onCheckedChange={() => setStatusFilter("")}
+                    checked={statusFilter === ""}
+                  >
+                    Semua
+                  </DropdownMenuCheckboxItem>
+                </DropdownMenuItem>
+                <DropdownMenuItem onSelect={() => setStatusFilter("Diterima")}>
+                  <DropdownMenuCheckboxItem
+                    onCheckedChange={() => setStatusFilter("Diterima")}
+                    checked={statusFilter === "Diterima"}
+                  >
+                    Diterima
+                  </DropdownMenuCheckboxItem>
+                </DropdownMenuItem>
+                <DropdownMenuItem onSelect={() => setStatusFilter("Menunggu")}>
+                  <DropdownMenuCheckboxItem
+                    onCheckedChange={() => setStatusFilter("Menunggu")}
+                    checked={statusFilter === "Menunggu"}
+                  >
+                    Menunggu
+                  </DropdownMenuCheckboxItem>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={() => setStatusFilter("Dibatalkan")}
+                >
+                  <DropdownMenuCheckboxItem
+                    onCheckedChange={() => setStatusFilter("Dibatalkan")}
+                    checked={statusFilter === "Dibatalkan"}
+                  >
+                    Dibatalkan
+                  </DropdownMenuCheckboxItem>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="flex items-center">
+              <Button className="ml-auto">
+                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                  Buat Reservasi
+                </span>{" "}
+                <PlusCircle className="ml-2 w-4 h-4" />
               </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              {table
-                .getAllColumns()
-                .filter((column) => column.getCanHide())
-                .map((column) => {
+            </div>
+          </div>
+          <Table>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
                   return (
-                    <DropdownMenuCheckboxItem
-                      key={column.id}
-                      className={cn(`capitalize`, {})}
-                      checked={column.getIsVisible()}
-                      onCheckedChange={(value) =>
-                        column.toggleVisibility(!!value)
-                      }
-                    >
-                      {column.id}
-                    </DropdownMenuCheckboxItem>
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
                   );
                 })}
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="">
-                <Filter className="w-4 h-4 mr-2" />
-                Filter Status
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem onSelect={() => setStatusFilter("")}>
-                <DropdownMenuCheckboxItem
-                  onCheckedChange={() => setStatusFilter("")}
-                  checked={statusFilter === ""}
-                >
-                  Semua
-                </DropdownMenuCheckboxItem>
-              </DropdownMenuItem>
-              <DropdownMenuItem onSelect={() => setStatusFilter("Diterima")}>
-                <DropdownMenuCheckboxItem
-                  onCheckedChange={() => setStatusFilter("Diterima")}
-                  checked={statusFilter === "Diterima"}
-                >
-                  Diterima
-                </DropdownMenuCheckboxItem>
-              </DropdownMenuItem>
-              <DropdownMenuItem onSelect={() => setStatusFilter("Menunggu")}>
-                <DropdownMenuCheckboxItem
-                  onCheckedChange={() => setStatusFilter("Menunggu")}
-                  checked={statusFilter === "Menunggu"}
-                >
-                  Menunggu
-                </DropdownMenuCheckboxItem>
-              </DropdownMenuItem>
-              <DropdownMenuItem onSelect={() => setStatusFilter("Dibatalkan")}>
-                <DropdownMenuCheckboxItem
-                  onCheckedChange={() => setStatusFilter("Dibatalkan")}
-                  checked={statusFilter === "Dibatalkan"}
-                >
-                  Dibatalkan
-                </DropdownMenuCheckboxItem>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-        <Table>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell
-                      className=""
-                      key={cell.id}
-                      onClick={() => {
-                        if (cell.row.original) {
-                          selectedReservation(row.original);
-                        }
-                      }}
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+            ))}
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell
+                        className=""
+                        key={cell.id}
+                        onClick={() => {
+                          if (cell.row.original) {
+                            selectedReservation(row.original);
+                          }
+                        }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
   );
 }
